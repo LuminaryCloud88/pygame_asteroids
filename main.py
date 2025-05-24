19cd4498c018debd1e141600e9100d9cbcf4ae3f
LuminaryCLoud88 <michaellevesque288@gmail.com>
2025-05-24 07:01:51 -0700

    "added shots"

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot  
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
     updatable_groups = pygame.sprite.Group()
     drawable_groups = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable_groups, drawable_groups)
     Asteroid.containers = (asteroids, updatable_groups, drawable_groups)
     AsteroidField.containers = (updatable_groups)
+    Shot.containers = (shots, updatable_groups, drawable_groups)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        updatable_groups.update(dt)    
+        updatable_groups.update(dt)   
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+            
         screen.fill("black")
 
         for obj in drawable_groups:
